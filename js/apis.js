0a4c6062670e051466068f06d3c524a58862c558
ferdiaz93 <fernandodiaz12@gmail.com>
2019-04-17 17:14:40 -0300

    Empezando el proyecto

---
+++ js/apis.js
@@ -0,0 +1,18 @@
+var posicionCentral = {lat: -34.608664, lng: -58.435572}
+var mapa // Mapa que vamos a modificar
+
+/* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
+
+// Inicializa el mapa con un valor de zoom y una locación en el medio
+function inicializarMapa () {
+  map = new google.maps.Map(document.getElementById("map"), {zoom: 12, center: posicionCentral})
+    /* Modificá la variable mapa con el constructor Map().
+    Tendrás que asignarle un valor de zoom y
+    un centro igual a la variable posicionCentral. */
+
+  geocodificadorModulo.inicializar()
+  marcadorModulo.inicializar()
+  direccionesModulo.inicializar()
+  lugaresModulo.inicializar()
+  streetViewModulo.inicializar()
+}
